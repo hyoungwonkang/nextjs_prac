6335a1d41aec9634a77e7381ead017d44f18c2ff
hyoungwonkang <hwplus@gmail.com>
2023-08-13 17:40:59 +0900

    [3.2 정적 자원 제공] 자동 이미지 최적화 - layout 속성, objectFit 속성 적용

--- components/Image.js
+++ components/Image.js
@@ -3,12 +3,20 @@ import Image from "next/image";
 function ImagePage() {
   return (
     <div>
-      <Image
-        src="https://recipe1.ezmember.co.kr/cache/recipe/2021/05/30/e457e303db8b14a604175933e15826051.jpg"
-        width={500}
-        height={200}
-        alt="장조림 버터 계란 비빔밥"
-      />
+      <div
+        style={{
+          width: 500,
+          height: 200,
+          position: "relative",
+        }}
+      >
+        <Image
+          src="https://recipe1.ezmember.co.kr/cache/recipe/2021/05/30/e457e303db8b14a604175933e15826051.jpg"
+          layout="fill"
+          objectFit="cover"
+          alt="장조림 버터 계란 비빔밥"
+        />
+      </div>
     </div>
   );
 }
