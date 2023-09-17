46f4fa4b00c148a00a8c47da9388f4d0b2f7100f
hyoungwonkang <hwplus@gmail.com>
2023-09-17 19:08:02 +0900

    _app.js 페이지 커스터마이징

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,13 +1,39 @@
+import { useContext } from "react";
 import Link from "next/link";
+import ThemeContext from "./themeContext";
+function Navbar2() {
+  //_app.js 연습을 위한 2 생성.
+  const { toggleTheme, theme } = useContext(ThemeContext);
+  const newThemeName = theme === "dark" ? "light" : "dark";
 
-function Navbar() {
   return (
-    <div>
-      <Link href="/about">Home</Link>
-      <Link href="/about">About</Link>
-      <Link href="/about">Contacts</Link>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        marginBottom: 25,
+      }}
+    >
+      <div>My Website</div>
+      <div>
+        <Link href="/">Home</Link>
+        <Link href="/about">About</Link>
+        <Link href="/contacts">Contacts</Link>
+        <button onClick={toggleTheme}>Set {newThemeName} theme</button>
+      </div>
     </div>
   );
 }
 
-export default Navbar;
+// function Navbar() {
+//   return (
+//     <div>
+//       <Link href="/about">Home</Link>
+//       <Link href="/about">About</Link>
+//       <Link href="/about">Contacts</Link>
+//     </div>
+//   );
+// }
+
+export default Navbar2;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,39 @@
+import { useState } from "react";
+import ThemeContext from "../components/themeContext";
+import Navbar2 from "../components/Navbar";
 import "../styles/globals.css";
 
+const themes = {
+  dark: {
+    background: "black",
+    color: "white",
+  },
+  light: {
+    background: "white",
+    color: "black",
+  },
+};
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const [theme, setTheme] = useState("light");
+  const toggleTheme = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div
+        style={{
+          width: "100%",
+          minHeight: "100vh",
+          ...themes[theme],
+        }}
+      >
+        <Navbar2 />
+        <Component {...pageProps} />
+      </div>
+    </ThemeContext.Provider>
+  );
 }
 
 export default MyApp;
