e4ad7a9846f11cd3983dd42899a26236e7c59151
hyoungwonkang <hwplus@gmail.com>
2023-12-19 23:20:13 +0900

    Auth0를 이용한 로그인

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,32 @@
-export default function Homepage() {
-  return <div> Homepage </div>;
+import { useUser } from '@auth0/nextjs-auth0/client';
+
+export default function Index() {
+  const { user, error, isLoading } = useUser();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>{error.message}</div>;
+  }
+
+  if (user) {
+    return (
+      <div>
+        <h1> Welcome back! </h1>
+        <p> You're logged in with the following email address: {user.email}!</p>
+        <a href="/api/auth/logout">Logout</a>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h1> Welcome, stranger! </h1>
+      <p>
+        Please <a href="/api/auth/login">Login</a>.
+      </p>
+    </div>
+  );
 }
